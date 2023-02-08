66767a4d71a28c807969bdd72f353ca3e80dddc4
Néstor Eduardo Miranda Vega <nemv.1311@gmail.com>
2023-02-08 16:34:39 -0600

    Actualización semanal Agenda

--- assets/packages/core/eventos.js
+++ assets/packages/core/eventos.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function() {
         center: 'title',
         right: 'dayGridMonth,dayGridWeek,dayGridDay'
       },
-      defaultDate: '2023-01-01',
+      defaultDate: '2023-02-01',
       locale: 'es',
       navLinks: true, // can click day/week names to navigate views
       editable: false,
@@ -30,6 +30,18 @@ document.addEventListener('DOMContentLoaded', function() {
           id: '',
           url: '',
           start: '2023-01-20'
+        },
+        {
+          title: 'Audiencias públicas',
+          id: 'Pachuca de Soto',
+          url: '<iframe src="https://www.facebook.com/plugins/video.php?height=424&href=https%3A%2F%2Fwww.facebook.com%2FHIDALGO.SEDAGRO%2Fvideos%2F1419303275477461%2F&show_text=true&width=560&t=0" width="100%" height="650" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>',
+          start: '2023-02-08'
+        },
+        {
+          title: 'Mesas de trabajo en Actopan',
+          id: 'Actopan',
+          url: '<iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FHIDALGO.SEDAGRO%2Fposts%2Fpfbid02JSGoErQhvxpCeR9TEoUYBo29cyua9E9TyJvT1fbh5mmS2eFz1JemTWdfxeVALH3kl&width=500&show_text=true&height=793&appId" width="500" height="793" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>',
+          start: '2023-02-05'
         }
       ],
       eventClick: function(item){
